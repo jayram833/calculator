f9033df0788cb8d2e9350a2c6ef7a2086bcb1a85
jayram833 <jaypasare96@gmail.com>
2023-02-05 11:16:56 +0530

    completed oprations

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
 "use strict";
+const digit = document.querySelectorAll(".dig");
+const operators = document.querySelectorAll(".op");
+
+let string = "";
+
+let buttons = document.querySelectorAll(".num");
+
+console.log(buttons);
+
+buttons.forEach(function (button) {
+  button.addEventListener("click", function (e) {
+    if (e.target.innerHTML === "=") {
+      string = eval(string);
+      console.log(string);
+      document.querySelector(".display_area").value = string;
+    } else if (e.target.innerHTML === "C") {
+      string = "";
+      document.querySelector(".display_area").value = string;
+    } else {
+      string = string + e.target.innerHTML;
+      document.querySelector(".display_area").value = string;
+    }
+  });
+});
